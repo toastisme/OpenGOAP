f04d5c652c57b641b82a21989f18cda805642ef3
davidmcdonagh <davidnmcdonagh@gmail.com>
2022-05-05 21:57:06 +0100

    Updated comments for GOAPGoal and IGoal.

--- Assets/Runtime/GOAP/GOAPGoal.cs
+++ Assets/Runtime/GOAP/GOAPGoal.cs
@@ -6,35 +6,72 @@ namespace GOAP{
 public class GOAPGoal : MonoBehaviour, IGoal
 {
 
+    /**
+     * \class GOAP.GOAPGoal
+     * A goal that can be achieved via a sequence of GOAPActions.
+     * Defined in terms of a priority, conditions required to achieve the goal,
+     * preconditions required to attempt the goal, and an actionLayer that determines
+     * what layer of GOAPActions will be considered for valid action plans.
+     */
+
     protected WorldState worldState;
     
-    /*
-     * Only GOAPActions with this actionLayer will be included during planning
-     */
+    // Only GOAPActions with this actionLayer will be included during planning
     public string actionLayer{get; protected set;} 
 
+    // What must be in worldState for the goal to be complete
     public Dictionary<string, bool> conditions{get; protected set;}
+
+    // What must be in worldState for the goal to be considered
+    public Dictionary<string, bool> preconditions{get; protected set;}
+
     public virtual void Setup(){
-        actionLayer = "All";
+
+        /*
+         * Called by GOAPPlanner when entering Play Mode.
+         */
+
+        actionLayer = "All"; // By default consider all GOAPActions to achieve conditions
         conditions = new Dictionary<string, bool>();
+        preconditions = new Dictionary<string, bool>();
         worldState = GetComponent<WorldState>();
     }
 
     public virtual float GetPriority(){
+
+        /*
+         * Assumed to be between 0f and 1f
+         */ 
+
         return 0f;
     }
 
     public virtual bool PreconditionsSatisfied(WorldState worldState){
-        return true;
+        // Will return true if preconditions are empty
+        return worldState.IsSubset(conditions);
     }
 
     public virtual bool ConditionsSatisfied(WorldState worldState){
         return worldState.IsSubset(conditions);
     }
 
-    public virtual void OnTick(){}
-    public virtual void OnActivate(){}
-    public virtual void OnDeactivate(){}
+    public virtual void OnTick(){
+        /*
+         * Called every frame by GOAPPlanner
+         */
+    }
+
+    public virtual void OnActivate(){
+        /*
+         * Called when selected by GOAPPlanner
+         */
+    }
+
+    public virtual void OnDeactivate(){
+        /*
+         * Called by GOAPPlanner when goal achieved or plan cancelled
+         */
+    }
 
 }
 }
--- Assets/Runtime/GOAP/Goals/IGoal.cs
+++ Assets/Runtime/GOAP/Goals/IGoal.cs
@@ -1,6 +1,11 @@
 namespace GOAP{
 public interface IGoal
 {
+    /**
+     * \interface GOAP.IGoal
+     * Basic interface for all GOAPGoals
+     */
+
     void Setup();
     float GetPriority(); // Between 0 and 1
     bool ConditionsSatisfied(WorldState worldState);
